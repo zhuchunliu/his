c69858529de138d90df41b57c506282f98820cc1
jimson-yu <1302109756@qq.com>
2018-01-25 13:51:36 +0800

    修复

--- jiLan-his/src/main/java/com/acmed/his/api/DicTypeApi.java
+++ jiLan-his/src/main/java/com/acmed/his/api/DicTypeApi.java
@@ -8,10 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,7 @@ import java.util.List;
  * @date 2018/1/25
  */
 @Api(tags = "字典分类管理")
+@RestController
 @RequestMapping("dicType")
 public class DicTypeApi {
 
