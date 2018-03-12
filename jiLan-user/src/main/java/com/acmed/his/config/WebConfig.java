165cbe1550db3ac2301ea8781c829903d059ab82
zhuchunliu <296898883@qq.com>
2018-03-12 14:38:07 +0800

    bug fix

--- jiLan-user/src/main/java/com/acmed/his/config/WebConfig.java
+++ jiLan-user/src/main/java/com/acmed/his/config/WebConfig.java
@@ -1,14 +1,17 @@
 package com.acmed.his.config;
 
+import com.acmed.his.exceptions.handler.ServiceExceptionHandler;
 import com.acmed.his.filter.RequestWrapperFilter;
 import com.acmed.his.filter.interceptor.GateInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.DispatcherType;
+import java.util.List;
 
 /**
  * Created by Darren on 2017-12-29
@@ -20,6 +23,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new GateInterceptor());
         super.addInterceptors(registry);
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        super.configureHandlerExceptionResolvers(exceptionResolvers);
+        exceptionResolvers.add(new ServiceExceptionHandler());
     }
 
     @Bean
