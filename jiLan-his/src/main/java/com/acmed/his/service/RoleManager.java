1071d91156eb87983c8925dc992a272bd2a22efd
zhuchunliu <296898883@qq.com>
2018-03-22 17:03:45 +0800

    bug fix

--- jiLan-his/src/main/java/com/acmed/his/service/RoleManager.java
+++ jiLan-his/src/main/java/com/acmed/his/service/RoleManager.java
@@ -41,20 +41,22 @@ public class RoleManager {
      */
     public List<Role> getRoleList(String isValid,Integer orgCode){
         Role role = new Role();
-        if(!StringUtils.isEmpty(isValid)){
-            role.setIsValid(isValid);
-        }
+
 //        role.setRemoved("0");
 //        role.setHideFlag(0);
 //        role.setOrgCode(orgCode);
         Example example = new Example(Role.class);
+        Example.Criteria criteria = null;
         if(null == orgCode){
-            example.createCriteria().andEqualTo("removed","0").
+            criteria = example.createCriteria().andEqualTo("removed","0").
                     andEqualTo("hideFlag",0).andIsNull("orgCode");
         }else{
-            example.createCriteria().andEqualTo("removed","0").
+            criteria = example.createCriteria().andEqualTo("removed","0").
                     andEqualTo("hideFlag",0).andEqualTo("orgCode",orgCode);
 
+        }
+        if(!StringUtils.isEmpty(isValid)){
+            criteria.andEqualTo("isValid",isValid);
         }
 
         return roleMapper.selectByExample(example);
