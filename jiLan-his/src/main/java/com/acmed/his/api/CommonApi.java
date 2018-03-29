c5f1bbd0067476ba84b2365bcf8fcc7341ddfe40
jimson-yu <1302109756@qq.com>
2018-03-29 17:50:48 +0800

    Bug-fix

--- jiLan-his/src/main/java/com/acmed/his/api/CommonApi.java
+++ jiLan-his/src/main/java/com/acmed/his/api/CommonApi.java
@@ -4,8 +4,10 @@ import com.acmed.his.support.WithoutToken;
 import com.acmed.his.util.ZXingCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -24,8 +26,8 @@ public class CommonApi {
     @ApiOperation(value = "文字转二维码")
     @WithoutToken
     @GetMapping("text2img")
-    public void getCodeImg(String txt, HttpServletResponse response){
+    public void getCodeImg(@ApiParam("文本") @RequestParam("text") String text, HttpServletResponse response){
 
-        ZXingCode.zxingCodeCreate(txt,200,200,response);
+        ZXingCode.zxingCodeCreate(text,200,200,response);
     }
 }
