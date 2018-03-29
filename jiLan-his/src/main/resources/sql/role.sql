f732d38767419f207f68a48d9e6b9c6f928c3752
jimson-yu <1302109756@qq.com>
2018-03-29 13:54:40 +0800

    role.sql维护

--- jiLan-his/src/main/resources/sql/role.sql
+++ jiLan-his/src/main/resources/sql/role.sql
@@ -1,6 +1,7 @@
 insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('admin','管理员',0,1);
 insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('jybsgqd','就医北上广渠道',0,1);
 insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('jybsgsc','就医北上广市场',0,1);
+insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('jybsgpt','就医北上广平台',0,1);
 insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('ys','医生',0,1);
 insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('hs','护士',0,1);
 insert into t_p_s_role(rolecode,rolename,removed,isvalid) values('ghy','挂号员',0,1);
@@ -25,6 +26,10 @@ insert into t_p_s_role_vs_permission(pid,rid)
 select id,(select id from t_p_s_role where rolecode = 'jybsgsc' )
  from t_p_s_permission t1 where percode ='sc' OR percode ='jybsg'
 
+-- 创建就医北上广市场角色
+insert into t_p_s_role_vs_permission(pid,rid)
+select id,(select id from t_p_s_role where rolecode = 'jybsgpt' )
+ from t_p_s_permission t1 where percode ='pt' OR percode ='jybsg'
 -- 传角色id 可以初始化成机构管理员
 insert into t_p_s_role_vs_permission(pid,rid)
         select id,#{rid}
